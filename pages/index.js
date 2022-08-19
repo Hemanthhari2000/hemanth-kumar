b542bcc7aa5dc0691c14ff261260c0a93b89445f
hemanthhari2000 <hemanthhari2000@gmail.com>
2022-08-19 22:21:55 +0530

    Project migrate from react to nextjs

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      <Text fontSize={"6xl"}>Hello, this is the Index Page!!!</Text>
+    </>
+  );
+}
