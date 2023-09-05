a34542d5722039f0e160ef96acfdcb904c6f3e98
hemanthhari2000 <hemanthhari2000@gmail.com>
2023-09-05 15:54:47 +0530

    Fix. link popping effect

--- components/Cards/workCard.js
+++ components/Cards/workCard.js
@@ -7,6 +7,7 @@ import {
   useBreakpointValue,
   useColorModeValue,
 } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const WorkCard = ({ work }) => {
   let translateOnHover = useBreakpointValue({
@@ -19,56 +20,58 @@ const WorkCard = ({ work }) => {
     const urlFormat = work["URL"].split(".").at(-1);
     isImage = ["png", "jpg", "jpeg"].includes(urlFormat);
   }
-
+  let workHref = `/works/${work.slug}`;
   return (
     <>
-      <Link
-        href={`/works/${work.slug}`}
-        overflow={"hidden"}
-        _hover={{
-          textDecoration: "none",
-        }}
-      >
-        <Box
-          w={"300px"}
-          mt={3}
-          cursor={"pointer"}
+      <NextLink href={workHref} passHref>
+        <Link
+          as={'a'}
           overflow={"hidden"}
           _hover={{
-            transform: translateOnHover,
-            transition: "0.8s",
-            transitionTimingFunction: "ease-out",
+            textDecoration: "none",
           }}
         >
-          <Box rounded={"xl"} overflow={"hidden"}>
-            {isImage ? (
-              <Image
-                src={work["URL"]}
-                fallbackSrc="https://via.placeholder.com/1920x1080"
-                alt="work"
-                layout={"fill"}
-              />
-            ) : (
-              <video autoPlay muted loop type="video/mp4" src={work["URL"]} />
-            )}
-          </Box>
-          <Box w={"100%"} textAlign={"center"} fontSize={"14px"}>
-            <Flex w={"100%"} justifyContent={"center"} alignItems={"center"}>
-              <Text as={"h1"} py={2} fontSize={"16px"} fontWeight={"bold"}>
-                {work["title"]}
+          <Box
+            w={"300px"}
+            mt={3}
+            cursor={"pointer"}
+            overflow={"hidden"}
+            _hover={{
+              transform: translateOnHover,
+              transition: "0.8s",
+              transitionTimingFunction: "ease-out",
+            }}
+          >
+            <Box rounded={"xl"} overflow={"hidden"}>
+              {isImage ? (
+                <Image
+                  src={work["URL"]}
+                  fallbackSrc="https://via.placeholder.com/1920x1080"
+                  alt="work"
+                  layout={"fill"}
+                />
+              ) : (
+                <video autoPlay muted loop type="video/mp4" src={work["URL"]} />
+              )}
+            </Box>
+            <Box w={"100%"} textAlign={"center"} fontSize={"14px"}>
+              <Flex w={"100%"} justifyContent={"center"} alignItems={"center"}>
+                <Text as={"h1"} py={2} fontSize={"16px"} fontWeight={"bold"}>
+                  {work["title"]}
+                </Text>
+              </Flex>
+              <Text
+                as={"p"}
+                p={2}
+                fontWeight={"semibold"}
+                color={useColorModeValue("blackAlpha.800", "whiteAlpha.800")}
+              >
+                {work["oneLineDescription"]}
               </Text>
-            </Flex>
-            <Text
-              as={"p"}
-              p={2}
-              fontWeight={"semibold"}
-              color={useColorModeValue("blackAlpha.800", "whiteAlpha.800")}
-            >
-              {work["oneLineDescription"]}
-            </Text>
+            </Box>
           </Box>
-        </Box>
-      </Link>
+        </Link >
+      </NextLink>
     </>
   );
 };
